c791e99b47d7bf3ade57dcae14570a09dd865508
sahil_thorat <sahil.thorat@healthfidelity.com>
2019-09-18 11:54:11 +0530

    Initial Commit

---
+++ mongodb-mysite/metadata.rb
@@ -0,0 +1,7 @@
+name        "mongodb-mysite"
+description 'Example cookbook for MongoDB Berkshelf based install'
+maintainer  "Ashok"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends 'mongodb'
